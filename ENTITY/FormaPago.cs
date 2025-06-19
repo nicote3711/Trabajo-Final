a263d0cd566dcdce59d1686a6da06cd1faf581bd
nicote3711 <nicobotbol_2@hotmail.com>
2025-06-19 16:10:15 -0300

    "Testeo de nueva rama"

---
+++ ENTITY/FormaPago.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY
+{
+    public class FormaPago
+    {
+        public int IdFormaPago { get; set; }
+        public string Descripcion { get; set; }
+        public decimal PorcentajeDescuentoRecargo { get; set; }
+    }
+}
